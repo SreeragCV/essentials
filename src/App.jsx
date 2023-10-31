f6a5472dfd8520ef896ee0b51b57df91b2fd4864
Sreerag Mohan <sreeraagmohancv@gmail.com>
2023-10-31 15:39:36 +0530

    useState Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,43 @@
-import { CORE_CONCEPTS } from './data';
-import Header from './components/Header/Header';
-import CoreConcepts from './components/CoreConcepts';
+import { CORE_CONCEPTS } from "./data";
+import Header from "./components/Header/Header";
+import CoreConcepts from "./components/CoreConcepts";
+import TabButton from "./components/TabButton";
+import { useState } from "react";
 
 function App() {
+
+  const[selectedTopic, setSelectedTopic] = useState('click a button');
+  
+  function handleClick(selectedButton) {
+    setSelectedTopic(selectedButton)
+  }
+
   return (
     <div>
-      <Header/>
+      <Header />
       <main>
-        <section id='core-concepts'>
+        <section id="core-concepts">
           <h2>Core Concepts</h2>
           <ul>
-          <CoreConcepts title={CORE_CONCEPTS[0].title} description={CORE_CONCEPTS[0].description} image={CORE_CONCEPTS[0].image}/>
-          <CoreConcepts {...CORE_CONCEPTS[1]}/>
-          <CoreConcepts {...CORE_CONCEPTS[2]}/>
-          <CoreConcepts {...CORE_CONCEPTS[3]}/>
+            <CoreConcepts
+              title={CORE_CONCEPTS[0].title}
+              description={CORE_CONCEPTS[0].description}
+              image={CORE_CONCEPTS[0].image}
+            />
+            <CoreConcepts {...CORE_CONCEPTS[1]} />
+            <CoreConcepts {...CORE_CONCEPTS[2]} />
+            <CoreConcepts {...CORE_CONCEPTS[3]} />
           </ul>
+        </section>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton onSelect={()=> handleClick('Components')}>Components</TabButton>
+            <TabButton onSelect={()=> handleClick('JSX')}>JSX</TabButton>
+            <TabButton onSelect={()=> handleClick('Props')}>Props</TabButton>
+            <TabButton onSelect={()=> handleClick('State')}>State</TabButton>
+          </menu>
+          {selectedTopic}
         </section>
       </main>
     </div>
