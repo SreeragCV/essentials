3af10bb91729074f4836748b59a33dded431f081
Sreerag Mohan <sreeraagmohancv@gmail.com>
2023-11-01 18:53:33 +0530

    mapped the data

--- src/App.jsx
+++ src/App.jsx
@@ -19,14 +19,9 @@ function App() {
         <section id="core-concepts">
           <h2>Core Concepts</h2>
           <ul>
-            <CoreConcepts
-              title={CORE_CONCEPTS[0].title}
-              description={CORE_CONCEPTS[0].description}
-              image={CORE_CONCEPTS[0].image}
-            />
-            <CoreConcepts {...CORE_CONCEPTS[1]} />
-            <CoreConcepts {...CORE_CONCEPTS[2]} />
-            <CoreConcepts {...CORE_CONCEPTS[3]} />
+            {CORE_CONCEPTS.map((e) => {
+              return <CoreConcepts key={e.title} {...e} />
+            })}
           </ul>
         </section>
         <section id="examples">
